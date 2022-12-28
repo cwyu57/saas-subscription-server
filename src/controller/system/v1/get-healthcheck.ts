47ec9873c409350512c78d88af000ce2ce667874
cwyu57 <cwyu57@gmail.com>
2022-12-28 23:50:44 +0800

    fix: health check response body

--- src/controller/system/v1/get-healthcheck.ts
+++ src/controller/system/v1/get-healthcheck.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const handler: express.Handler = async (req, res, next) => {
-  res.status(200).json('OK');
+  res.status(200).json({ status: 'OK' });
 };
 
 export default handler;
