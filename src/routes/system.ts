bff0a367dcaa25450d738f0aa8e61db84ba67f82
cwyu57 <cwyu57@gmail.com>
2022-12-28 23:37:05 +0800

    feat: add express async handler

--- src/routes/system.ts
+++ src/routes/system.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
 
 import v1GetHealthcheck from '../controller/system/v1/get-healthcheck';
 
 const router = express.Router();
 
-router.get('/system/v1/healthcheck', v1GetHealthcheck);
+router.get('/system/v1/healthcheck', asyncHandler(v1GetHealthcheck));
 
 export default router;
