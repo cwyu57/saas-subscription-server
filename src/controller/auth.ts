8f35bee39bb00d68824137ad0e2bc0a126fc1c5d
cwyu57 <cwyu57@gmail.com>
2023-01-03 18:27:10 +0800

    feat: implement isAuthenticated middleware

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -3,6 +3,27 @@ import { StatusCodes, ReasonPhrases } from 'http-status-codes';
 import { config } from '../config';
 import { errorCodes } from '../const';
 import { ResponseError } from '../entity';
+import { JwtService } from '../service/jwt';
+
+export const isAuthenticated: express.Handler = async (req, res, next) => {
+  if (!req.headers.authorization) {
+    return next(
+      new ResponseError(
+        errorCodes.MISSING_AUTHORIZATION,
+        StatusCodes.BAD_REQUEST,
+        ReasonPhrases.BAD_REQUEST,
+      ),
+    );
+  }
+
+  const authorization = req.headers.authorization as string;
+  const token = authorization.split(' ')[1];
+
+  const decodedToken = JwtService.verifyToken(token);
+  console.log(decodedToken);
+  (req as any).user = decodedToken;
+  return next();
+};
 
 export const systemApiKeyMiddleware: express.Handler = async (
   req,
