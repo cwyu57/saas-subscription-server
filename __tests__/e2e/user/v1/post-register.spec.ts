a8fb939ed17c93eef4b1af967411b3c66a7480c1
cwyu57 <cwyu57@gmail.com>
2023-01-04 20:59:06 +0800

    feat: local test

---
+++ __tests__/e2e/user/v1/post-register.spec.ts
@@ -0,0 +1,62 @@
+import { Server } from 'http';
+import supertest from 'supertest';
+
+import app from '../../../../src/app';
+import { saasSubscriptionModels } from '../../../../src/container';
+import { sequelize } from '../../../../src/loader';
+
+describe('POST /member/v1/phone-register', () => {
+  let request: supertest.SuperTest<supertest.Test>;
+  let server: Server;
+
+  beforeAll(done => {
+    if (
+      process.env.NODE_ENV === 'jest' &&
+      process.env.DB_DATABASE === 'saas-subscription'
+    ) {
+      // WARINING: force sync database
+      console.log('start syncing database');
+      saasSubscriptionModels.sync({ force: true }, async () => {
+        server = app.listen(3000, () => {
+          request = supertest(server);
+          done();
+        });
+      });
+    }
+  });
+
+  afterAll(done => {
+    server.close(async () => {
+      await sequelize.close();
+      done();
+    });
+  });
+
+  it('expect status 200', done => {
+    request
+      .post('/user/v1/register')
+      .send({
+        email: 'cwyu57@gmail.com',
+        name: 'cwyu57',
+        password: 'abc',
+      })
+      .expect(200)
+      .then(response => {
+        expect(response.body).toMatchObject({
+          data: {
+            user: {
+              email: 'cwyu57@gmail.com',
+              name: 'cwyu57',
+            },
+          },
+        });
+        expect(response.body.data).toHaveProperty('accessToken');
+        expect(response.body.data).toHaveProperty('refreshToken');
+        expect(response.body.data.user).toHaveProperty('id');
+        expect(response.body.data.user).toHaveProperty('email');
+        expect(response.body.data.user).toHaveProperty('name');
+        done();
+      })
+      .catch(err => done(err));
+  });
+});
